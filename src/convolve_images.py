1a8bff8edbc8b939e23aa73c753a2e9493f408e0
Sam Cutler <secutler@umass.edu>
2023-04-21 12:52:29 -0400

    convolving smart now

--- src/convolve_images.py
+++ src/convolve_images.py
@@ -26,7 +26,13 @@ else:
     convolve_kwargs = {}
 
 for filename in SCI_FILENAMES:
-    if os.path.exists(filename.replace('.fits', f'_{KERNEL}-matched.fits')): continue
+    outsciname = filename.replace(f'{SKYEXT}.fits', f'{SKYEXT}_{KERNEL}-matched.fits')
+    outwhtname=filename.replace(f'_sci{SKYEXT}.fits', f'_wht_{KERNEL}-matched.fits')
+    if os.path.exists(outsciname) and os.path.exists(outwhtname):
+        print(outsciname, outwhtname)
+        print(f'Convolved images exist, I will not overwrite')
+        continue
+
     # if f'sci{SKYEXT}' not in filename: continue
     for band in FILTERS:
         if band in filename:
@@ -49,21 +55,32 @@ for filename in SCI_FILENAMES:
 
         weight = hdul_wht[0].data
 
-        # print(np.shape(hdul[0].data))
-        print('Running convolution...')
-        hdul[0].data = convolve_func(hdul[0].data, kernel, **convolve_kwargs).astype(np.float32)
-        hdul[0].data[weight==0] = 0.
-        outfilename = filename.replace(DIR_IMAGES, DIR_OUTPUT).replace(f'{SKYEXT}.fits', f'{SKYEXT}_{KERNEL}-matched.fits')
-        hdul.writeto(outfilename, overwrite=True)
-        print('Wrote file to ', outfilename)
+        if not os.path.exists(outsciname):
+            print('Running science image convolution...')
+            hdul[0].data = convolve_func(hdul[0].data, kernel, **convolve_kwargs).astype(np.float32)
+            hdul[0].data[weight==0] = 0.
+            hdul.writeto(outsciname, overwrite=True)
+            print('Wrote file to ', outsciname)
+            hdul.close()
+        else:
+            print(outsciname)
+            print(f'{band.upper} convolved science image exists, I will not overwrite')
 
-        err = np.where(weight==0, 0, 1/np.sqrt(weight))
-        err_conv = convolve_func(err, kernel, **convolve_kwargs).astype(np.float32)
-        hdul_wht[0].data = np.where(err_conv==0, 0, 1./(err_conv**2))
-        hdul_wht[0].data[weight==0] = 0.
-        hdul_wht.writeto(filename.replace(DIR_IMAGES, DIR_OUTPUT).replace(f'_sci{SKYEXT}.fits', f'_wht_{KERNEL}-matched.fits'), overwrite=True)
+        if not os.path.exists(outwhtname):
+            print('Running weight image convolution...')
+            err = np.where(weight==0, 0, 1/np.sqrt(weight))
+            err_conv = convolve_func(err, kernel, **convolve_kwargs).astype(np.float32)
+            hdul_wht[0].data = np.where(err_conv==0, 0, 1./(err_conv**2))
+            hdul_wht[0].data[weight==0] = 0.
+            hdul_wht.writeto(outwhtname, overwrite=True)
+            print('Wrote weight file to ', outwhtname)
+        else:
+            print(outwhtname)
+            print(f'{band.upper} convolved weight image exists, I will not overwrite')
+
+
         print(f'Finished in {time.time()-tstart:2.2f}s')
 
     else:
-        # hdul.writeto(filename.replace(DIR_IMAGES, DIR_OUTPUT).replace(f'{SKYEXT}.fits', f'{SKYEXT}_{KERNEL}-matched.fits'), overwrite=True)
+        hdul.writeto(filename.replace(DIR_IMAGES, DIR_OUTPUT).replace(f'{SKYEXT}.fits', f'{SKYEXT}_{KERNEL}-matched.fits'), overwrite=True)
         hdul_wht.writeto(filename.replace(DIR_IMAGES, DIR_OUTPUT).replace(f'_sci{SKYEXT}.fits', f'_wht_{KERNEL}-matched.fits'), overwrite=True)
